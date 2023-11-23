72ca558790796e8c6421fb0119a9c8fdef356b7e
Lokesh Saipureddi <saipureddilokesh@gmail.com>
2023-11-22 19:41:12 -0500

    Time: 34 ms (81.90%) | Memory: 16.4 MB (24.72%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -12,6 +12,6 @@ class Solution:
             return False
         if p.val!=q.val:
             return False
-        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)                           
+        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)       
         
         
