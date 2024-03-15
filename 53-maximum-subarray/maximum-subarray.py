3633e513533adabf77dc91bf6501ab699f31b799
Lokesh Saipureddi <saipureddilokesh@gmail.com>
2024-03-15 11:12:09 -0400

    Time: 535 ms (54.39%) | Memory: 30.9 MB (79.53%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,10 +1,13 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        k=float(-inf)
-        sum=0
+        k = -pow(10,6)
+        sum = 0
         for i in range(0,len(nums)):
-            sum=sum+nums[i]
-            k=max(k,sum)
-            if sum<0:
-                sum=0
-        return k
+            sum = sum + nums[i]
+            k = max(k, sum)
+            if sum < 0:
+                sum = 0
+        return k    
+
+
+        
